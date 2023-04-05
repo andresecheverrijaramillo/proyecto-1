9edaf94c5fb7ef63c4783920496c27e4a8c0c745
andresecheverrijaramillo <aecheverrj@eafit.edu.co>
2023-04-04 21:27:04 -0500

    update
    update

--- microservicios/ClientService/src/client.js
+++ microservicios/ClientService/src/client.js
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 import grpc from '@grpc/grpc-js';
 import protoLoader from '@grpc/proto-loader';
 import fs from 'fs';
+
 dotenv.config()
+
 const PROTO_PATH = process.env.PROTO_PATH;
 const REMOTE_HOST = process.env.REMOTE_HOST;
 const REMOTE_HOST2 = process.env.REMOTE_HOST2;
@@ -11,21 +13,26 @@ const MOM = process.env.MOM;
 // definir atributos para la conexion con el servidor
 const packageDefinition = protoLoader.loadSync(
   PROTO_PATH,
-  {keepCase: true,
-   longs: String,
-   enums: String,
-   defaults: true,
-   oneofs: true
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
   });
-// se crea el constructor para crear conexiones con el servidor
+
+console.info("Consumer service is started...");
+const proto = grpc.loadPackageDefinition(packageDefinition);
+const server = new grpc.Server();
 
 const replicationService = grpc.loadPackageDefinition(packageDefinition).ReplicationService;
 const productService = grpc.loadPackageDefinition(packageDefinition).ProductService;
 const inventoryService = grpc.loadPackageDefinition(packageDefinition).InventoryService;
-const proto = grpc.loadPackageDefinition(packageDefinition);
-const server = new grpc.Server();
+console.log(grpc.loadPackageDefinition(packageDefinition).ReplicationService);
+console.log(replicationService);
+
 // se crea la coexion con el servidor
-const externalServer = new replicationService(MOM,grpc.credentials.createInsecure());
+const externalServer = new replicationService(MOM, grpc.credentials.createInsecure());
 
 function checkServer() {
   externalServer.waitForReady(new Date().setTime(new Date().getTime() + 5000), (err) => {
@@ -65,94 +72,26 @@ function addUser(userName, userPassword) {
 }
 
 function findUser(userName, userPassword) {
-  const users = JSON.parse(fs.readFileSync('users.json', 'utf8'));  
+  const users = JSON.parse(fs.readFileSync('users.json', 'utf8'));
   const foundUser = users.user.find(user => user.name === userName && user.password === userPassword);
-return foundUser;}
+  return foundUser;
+}
 
-function addToQueue(userName, method, variables){
+function addToQueue(userName, method, variables) {
   const existingJSON = JSON.parse(fs.readFileSync('queues.json', 'utf-8'));
-  const newJSON = {[userName]: [{"method": method,"variables": variables}]}
-  const combinedJSON = {...existingJSON, ...newJSON};
+  const newJSON = { [userName]: [{ "method": method, "variables": variables }] }
+  const combinedJSON = { ...existingJSON, ...newJSON };
   fs.writeFileSync('queues.json', JSON.stringify(combinedJSON));
 }
 
-server.addService(proto.QueueService.service,{
-  add: (call, callback) => {
-    const method = call.request.method; 
-    const password = call.request.password; 
-    const user = call.request.user; 
-    const variables = call.request.variables;
-    if(findUser(user,password)){
-      switch (method) {
-        case 1:{
-          addUser(user,password);
-        }
-        case 2:{
-          client2.GetInventory((err, data) => {
-            if(err){
-              callback(null,"0");
-            } else {
-              callback(null,data);
-            }
-           })
-        }
-        case 3:{
-          const userArray = variables[username];
-          const userObj = userArray[0].variables; 
-          const idProduct=userObj.id_product;
-          const productName=userObj.name;
-          client2.addProducts({id:idProduct,name:productName},(err, data) => {
-            if(err){
-              callback(null,"0");
-            } else {
-              callback(null,data);
-            }
-           })
-        }
-        case 4:{
-          client2.GetInventoryLastIdd((err, data) => {
-            if(err){
-              callback(null,"0");
-            } else {
-              callback(null,data);
-            }
-           })
-        }
-        case 5:{
-          const userArray = variables[username];
-          const userObj = userArray[0].variables; 
-          const idProduct=userObj.id_product;
-          const productName=userObj.name;
-          client.AddProduct({id:idProduct,name:productName,userName:username},(err, data) => {
-            if(err){
-              callback(null,"0");
-            } else {
-              callback(null,data);
-            }
-           })
-        }
-        case 6:{
-          const userArray = variables[username];
-          const userObj = userArray[0].variables; 
-          const idProduct=userObj.id_product;
-          const productName=userObj.name;
-          client.DeleteProduct({id:idProduct,name:productName,userName:username},(err, data) => {
-            if(err){
-              callback(null,"0");
-            } else {
-              callback(null,data);
-            }
-           })}
-      }
-    }
-    else{
-      callback(null,"0");
-    }
+server.addService(proto.ReplicationService.service, {
+  GetInventory2: (call, callback) => {
+    callback(null, "0");
   },
 })
 
 server.bindAsync(
-  "127.0.0.1:8082",grpc.ServerCredentials.createInsecure(),
+  "127.0.0.1:8082", grpc.ServerCredentials.createInsecure(),
   (error, port) => {
     console.log("Server running at 127.0.0.1:8082");
     server.start();
